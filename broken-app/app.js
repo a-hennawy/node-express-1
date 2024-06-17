3243d4975b5d113a16a2d65ad818c0a1cf7e8d88
Ahmed <a.hennawy97@hotmail.com>
2024-06-17 19:52:46 +0300

    added final revision of files

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,47 @@
-const express = require('express');
-let axios = require('axios');
-var app = express();
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const ExpressError = require("./expressError");
 
-app.post('/', function(req, res, next) {
+app.use(express.json());
+app.post("/", async function (req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
+    let results = await Promise.all(
+      req.body.developers.map(async (d) => {
+        try {
+          const resp = await axios.get(`https://api.github.com/users/${d}`);
+          if (resp.status === 404) throw new ExpressError("Not Found", 404);
+          return resp.data;
+        } catch (err) {
+          if (err.response && err.response.status === 404) {
+            throw new ExpressError(`User ${d} NOT FOUND`, 404);
+          } else {
+            throw new ExpressError("Failed to fetch data from Github", 500);
+          }
+        }
+      })
+    );
+    let out = results.map((r) => ({ name: r.name, bio: r.bio }));
+    return res.json(out);
+  } catch (err) {
     next(err);
   }
 });
 
-app.listen(3000);
+app.use((req, res, next) => {
+  const notFoundError = new ExpressError("Not Found", 404);
+  return next(notFoundError);
+});
+
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  let message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Running app on port 3000");
+});
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,9 @@
 # Broken App Issues
+
+1- replaced var and let with const.
+
+2- app.listen was missing the callback function.
+
+3- added error handling
+
+4- corrected the use of jsonifying in Express.
